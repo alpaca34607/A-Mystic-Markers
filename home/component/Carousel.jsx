e4c7435770e2b144c667a358501315b853739742
alpaca34607 <alpaca34607@gmail.com>
2024-12-07 16:58:39 +0800

    1207

--- home/component/Carousel.jsx
+++
@@ -1,8 +0,0 @@
-const angle = 0;
-function galleryspin(sign) { 
-spinner = document.querySelector("#spinner");
-if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
-spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
-}
-
-export default  galleryspin
